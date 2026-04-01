f403a94472656e66579b04de6c785e46c6e290b3
kalish <ukalish@opentext.com>
2026-04-01 15:42:17 +0300

    wip

--- src/ui/themes.ts
+++ src/ui/themes.ts
@@ -9,61 +9,61 @@ export const THEMES: Theme[] = [
   {
     id: '108',
     vars: {
-      '--surface-bg': 'hsl(108, 22%, 14%)',
+      '--surface-bg': 'hsl(108, 22%, 13.99%)',
     },
   },
   {
     id: '144',
     vars: {
-      '--surface-bg': 'hsl(144, 22%, 14%)',
+      '--surface-bg': 'hsl(144, 22%, 14.04%)',
     },
   },
   {
     id: '180',
     vars: {
-      '--surface-bg': 'hsl(180, 22%, 14%)',
+      '--surface-bg': 'hsl(180, 22%, 13.81%)',
     },
   },
   {
     id: '216',
     vars: {
-      '--surface-bg': 'hsl(216, 22%, 16%)',
+      '--surface-bg': 'hsl(216, 22%, 16.70%)',
     },
   },
   {
     id: '252',
     vars: {
-      '--surface-bg': 'hsl(252, 22%, 17%)',
+      '--surface-bg': 'hsl(252, 22%, 18.79%)',
     },
   },
   {
     id: '288',
     vars: {
-      '--surface-bg': 'hsl(288, 22%, 16%)',
+      '--surface-bg': 'hsl(288, 22%, 17.44%)',
     },
   },
   {
     id: '324',
     vars: {
-      '--surface-bg': 'hsl(324, 22%, 16%)',
+      '--surface-bg': 'hsl(324, 22%, 17.28%)',
     },
   },
   {
     id: '0',
     vars: {
-      '--surface-bg': 'hsl(0, 22%, 17%)',
+      '--surface-bg': 'hsl(0, 22%, 17.65%)',
     },
   },
   {
     id: '36',
     vars: {
-      '--surface-bg': 'hsl(36, 22%, 15%)',
+      '--surface-bg': 'hsl(36, 22%, 14.76%)',
     },
   },
   {
     id: '72',
     vars: {
-      '--surface-bg': 'hsl(72, 22%, 14%)',
+      '--surface-bg': 'hsl(72, 22%, 13.41%)',
     },
   },
 ];
